a00a1d591ab851f7f0d994d705be0faa726e8316
MORTEX <58103075+mr-mortex@users.noreply.github.com>
2022-07-23 11:57:33 +0200

    Delete calculadora.py

--- CALCULADORA/calculadora.py
+++
@@ -1,8 +0,0 @@
-import PySimpleGUI as sg
-
-layout = [[sg.Text('Enter a Number')],
-          [sg.Input()],
-                    [sg.OK()] ]
-
-event, values = sg.Window('Enter a number example', layout).Read()
-sg.Popup(event, values[0])
